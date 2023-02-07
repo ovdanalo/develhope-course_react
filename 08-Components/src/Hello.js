86b0060e87d022f3a8c1b08d7b39aa7019bb9f97
ovdanalo <104578306+ovdanalo@users.noreply.github.com>
2023-02-07 11:54:13 +0100

    add Components exercise

---
+++ 08-Components/src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <>
+      <h1>Hello, World!</h1>
+      <p>What a beautiful day!</p>
+      </>
+    );
+  }
+}
+
+export default Hello;
